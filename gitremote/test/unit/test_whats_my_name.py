72ccd0c4e5418dd7533aa26667905c718ba8a413
wtccjava <jparks@waketech.edu>
2019-07-12 14:19:58 -0400

    added gitremote code

---
+++ gitremote/test/unit/test_whats_my_name.py
@@ -0,0 +1,7 @@
+import pytest
+
+from gitremote.whats_my_name import my_name_is
+
+
+def test_my_name_is():
+    assert "Joe" == my_name_is()
---
+++ gitremote/whats_my_name.py
@@ -0,0 +1,5 @@
+def my_name_is():
+    return "Joe"
+
+def main():
+    print(my_name_is())
