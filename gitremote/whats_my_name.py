05a14e2cf786a6331b17c890b2426dfa11a437d5
palaheel76 <palaheel76@gmail.com>
2019-07-24 13:59:06 -0400

    change my name to Joseph

--- gitremote/whats_my_name.py
+++ gitremote/whats_my_name.py
@@ -1,8 +1,6 @@
 def my_name_is():
-    return "Joseph"
+    return "Sophie"
 
 
-# palaheel76 put in this comment 7/21/19 6:00pm
-# palaheel76 put in this comment 7/22/19 at 1:43pm
 def main():
     print(my_name_is())
