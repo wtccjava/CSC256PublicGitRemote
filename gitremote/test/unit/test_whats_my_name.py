4b2fe9845aba070254958db1dde01fdc17831379
mjneiditch <mjneiditch@my.waketech.edu>
2020-10-19 19:30:38 -0400

    updates unit testing for new name

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,4 @@ from gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr." == my_name_is()
+    assert "Lewis Carroll" == my_name_is()
