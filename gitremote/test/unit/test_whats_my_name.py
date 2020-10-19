795abb6d3a67f66b2c0d665699162c09a192a3b9
smbannoura2 <smbannoura2@my.waketech.edu>
2020-10-19 14:58:33 -0400

    Tester name changed to the required name to match py file

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,4 @@ from CSC256PublicGitRemote.gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "smbannoura2" == my_name_is()
+    assert "Stavros Bannoura" == my_name_is()
