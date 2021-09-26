4d5368c09cd07585eb91cd96c350ad1902e2e180
bsrogers2 <bsrogers2@my.waketech.edu>
2021-09-26 15:39:44 -0400

    Changed name, added .travis.yml

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,4 @@ from gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "bsrogers2" == my_name_is()
+    assert "Benjamin Rogers" == my_name_is()
--- gitremote/whats_my_name.py
+++ gitremote/whats_my_name.py
@@ -1,5 +1,5 @@
 def my_name_is():
-    return "bsrogers2"
+    return "Benjamin Rogers"
 
 
 # palaheel76 doesn't want any comments
